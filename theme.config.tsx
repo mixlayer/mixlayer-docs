5c49cf2412d4217681ccddd1344c11d3cdc5f1cc
Zack Angelo <zackangelo@gmail.com>
2025-06-30 14:18:50 -0700

    wip

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,23 +10,22 @@ const config: DocsThemeConfig = {
       <MixlayerLogo />
     </span>
   ),
+  nextThemes: {
+    defaultTheme: "dark",
+  },
   project: {
     link: "https://github.com/mixlayer/mixlayer-docs",
   },
   chat: {
     link: "https://discord.gg/dWzh65e7tP",
   },
-  banner: {
-    key: "beta-access-1",
-    text: "⚠️ Mixlayer is currently in open beta. Please excuse us if you encounter any issues while we get things ready.",
-  },
   docsRepositoryBase: "https://github.com/mixlayer/mixlayer-docs/blob/main",
   footer: {
-    text: "Mixlayer Labs, Inc © 2025",
+    text: "Mixlayer Labs Inc © 2025",
   },
   useNextSeoProps() {
     return {
-      description: "Mixlayer: Custom AI using simple JS",
+      description: "Mixlayer: Limitless AI inference",
       titleTemplate: "%s | Mixlayer Documentation",
     };
   },
