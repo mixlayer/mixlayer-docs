9636103d1d93eb4da1c78ecb5c26d33d09fd4d2f
Zack Angelo <zackangelo@gmail.com>
2025-07-08 22:57:32 -0700

    add posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,28 @@
-import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+import { Analytics } from "@vercel/analytics/react";
+import { useEffect } from "react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+      defaults: "2025-05-24",
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === "development") posthog.debug();
+      },
+    });
+  }, []);
+
   //@ts-ignore
   return (
-    <>
+    <PostHogProvider client={posthog}>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </PostHogProvider>
   );
 }
