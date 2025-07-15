464552f269fb9e1511a558df5554bfb2c0f35593
Zack Angelo <zackangelo@gmail.com>
2025-07-15 11:30:29 -0700

    rearrange left nav, add openai api docs

--- pages/_meta.tsx
+++ pages/_meta.tsx
@@ -3,10 +3,29 @@ import { Meta } from "nextra";
 const meta: Meta = {
   index: "Introduction",
   quickstart: "Quickstart",
+  "---": {
+    type: "separator",
+    title: <div className="text-zinc-50 uppercase -mb-2">SDK</div>,
+  },
   sequences: {
-    title: "Using Sequences",
+    title: "Basics",
+  },
+  "tool-calls": {
+    title: "Tools",
+  },
+  forking: {
+    title: "Forking",
+  },
+
+  "openai-rest-api": {
+    title: "OpenAI REST API",
+  },
+  "-----": {
+    type: "separator",
+    title: <div className="text-zinc-50 uppercase -mb-2">Platform</div>,
   },
   pricing: "Pricing",
+  api_keys: "API Keys",
 };
 
 export default meta;
